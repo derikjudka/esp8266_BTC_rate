57dfcf1c2d5150c00fe4e02cdc03d10136e6113e
derikjudka <derik.judka@gmail.com>
2022-03-18 14:59:39 -0500

    time delay fix

---
+++ lcd_gpio.py
@@ -0,0 +1,69 @@
+import utime
+import machine
+
+RS = machine.Pin(0, machine.Pin.OUT)
+E = machine.Pin(1, machine.Pin.OUT)
+D0 = machine.Pin(2, machine.Pin.OUT)
+D1 = machine.Pin(3, machine.Pin.OUT)
+D2 = machine.Pin(6, machine.Pin.OUT)
+D3 = machine.Pin(8, machine.Pin.OUT)
+D4 = machine.Pin(11, machine.Pin.OUT)
+D5 = machine.Pin(13, machine.Pin.OUT)
+D6 = machine.Pin(14, machine.Pin.OUT)
+D7 = machine.Pin(15, machine.Pin.OUT)
+
+def enable_pulse():
+    E.value(1)
+    utime.sleep_us(40)
+    E.value(0)
+    utime.sleep_us(40)
+
+def send_LCD(binary_num):
+    D0.value((binary_num & 0b00000001) >> 0)
+    D1.value((binary_num & 0b00000010) >> 1)
+    D2.value((binary_num & 0b00000100) >> 2)
+    D3.value((binary_num & 0b00001000) >> 3)
+    D4.value((binary_num & 0b00010000) >> 4)
+    D5.value((binary_num & 0b00100000) >> 5)
+    D6.value((binary_num & 0b01000000) >> 6)
+    D7.value((binary_num & 0b10000000) >> 7)
+    enable_pulse()
+
+def configure_LCD():
+    RS.value(0)
+    #necessary special case of 'Function set' 
+    send_LCD(0b00110000)
+    send_LCD(0b00110000)
+    send_LCD(0b00110000)
+    
+    #configure 8-bit bus, 2 lines
+    send_LCD(0b00111000)
+
+    #display on, cursor off, blink off
+    send_LCD(0b00001100)
+    
+    #entry mode set
+    send_LCD(0b00000100)
+    
+    #clear screen
+    send_LCD(0b00000001)
+    utime.sleep_ms(5)
+
+def second_row():
+    RS.value(0)
+    send_LCD(0b10101001)
+    utime.sleep(1)
+    
+def clear_display():
+    RS.value(0)
+    send_LCD(0b00000001)
+    utime.sleep(2)
+    
+def return_home():
+    RS.value(0)
+    send_LCD(0b00000010)
+    utime.sleep(2)
+             
+def write_LCD(binary):
+    RS.value(1)
+    send_LCD(binary)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import machine
 import time
 import json
+import lcd_gpio
 
 uart = machine.UART(1,115200)
 
@@ -8,6 +9,7 @@ SSID = "" #enter wifi ssid here
 PASSWORD = "" #enter wifi password here
 
 def main():
+    lcd_gpio.configure_LCD()
     setup_esp()
     connect_wifi()
     time.sleep(5)
@@ -16,17 +18,26 @@ def main():
     while True:
         check_wifi_connection()
         output = send_request()
+        
         if output != -1:
             rate = get_rate(output)
             update_time = get_time(output)
-            print("BTC: {} USD\nUpdated: {}".format(rate, update_time))
+        
+            new_rate = "BTC: {} USD".format(str(int(rate)))
+            new_time = update_time[16:21:1]
+            
+            display(new_rate)
+            lcd_gpio.second_row()
+            display(new_time)
         else:
+            display('bad data')
             uart.write('AT+CIPCLOSE\r\n')
             time.sleep(2)
             close_conn = serial_read()
             time.sleep(2)
-            tcp_init() 
-        time.sleep(15)
+            tcp_init()
+            
+        time.sleep(30)
 
 def serial_read():
     data = bytes()
@@ -49,89 +60,53 @@ def setup_esp():
 def connect_wifi():
     uart.write('AT+CWJAP="'+SSID+'","'+PASSWORD+'"\r\n')
     time.sleep(6)
-    wifi_info = serial_read()
-    wifi_info = wifi_info.decode('utf-8')
+    wifi_info = serial_read().decode('utf-8')
     return wifi_info
 
 def check_wifi_connection():
     uart.write('AT+CWJAP?\r\n')
     time.sleep(5)
-    wifi_status = serial_read()
-    wifi_status = wifi_status.decode('utf-8')
+    wifi_status = serial_read().decode('utf-8')
     while wifi_status.find('No AP') != -1:
-        print('NO WIFI...MUST RECONNECT')
+        #NO WIFI...MUST RECONNECT
         connect_wifi()
         
 def get_esp_ip():
     uart.write('AT+CIFSR\r\n')
     time.sleep(5)
-    esp_ip = serial_read()
-    esp_ip = esp_ip.decode('utf-8')
+    esp_ip = serial_read().decode('utf-8')
     return esp_ip
 
 def tcp_init():
     uart.write('AT+CIPSTART="TCP","api.coindesk.com",80\r\n')
     time.sleep(2)
-    tcp_info = serial_read()
-    tcp_info = tcp_init.decode('utf-8')
+    tcp_info = serial_read().decode('utf-8')
     return tcp_info
     
 #send_request returns -1 for bad request
 def send_request():
     if uart.any() > 0:
         uart.read()
+    
+    time.sleep(3)
         
     send = 'GET /v1/bpi/currentprice.json HTTP/1.1\r\n'
-    send += 'Host: api.coindesk.com\r\n'
-    send += '\r\n'
+    send += 'Host: api.coindesk.com\r\n\r\n'
     msg_length = str(len(send))
     
     uart.write('AT+CIPSEND='+msg_length+'\r\n')
-    time.sleep(1)
-    info_size = serial_read()
+    time.sleep(0.01)
+    clear_buf = serial_read()
             
-    uart.write(send+'\r\n')
-    time.sleep(0.1)
-    buf1 = serial_read()
+    uart.write(send)
+    buf = ''
+    for i in range(10):
+        buf += serial_read().decode('utf-8')
+        time.sleep(0.01)
     
-    count1 = 0
-    while buf1.decode('utf-8').find('+IPD') == -1:
-        buf1 = serial_read()
-        count1 += 1
-        time.sleep(0.5)
-        if count1 > 10:
-            return -1
-    buf1 = buf1.decode('utf-8')
-    
-    time.sleep(0.002)
-    uart.write('+++')
-    time.sleep(0.002)
-
-    buf2 = serial_read()
-    
-    if (buf1.find('{"time"') == -1 and buf1.find('}}}') == -1):
-        count2 = 0
-        while (buf2.decode('utf-8').find('}}}') == -1):
-            buf2 += serial_read()
-            count2 += 1
-            time.sleep(0.5)
-            if count2 > 10:
-                return -1
-        buf2 = buf2.decode('utf-8')
-        if (buf2.find('{"time"') == -1):
-            return -1
-    
-        time.sleep(1)
-    
-    if uart.any() > 0:
-        uart.read()
-    time.sleep(0.5)
-    
-    if isinstance(buf2, str):
-        output = buf1 + buf2
-    else:
-        output = buf1 + buf2.decode('utf-8')
-    return output
+    if (buf.find('{"time"') == -1 or buf.find('}}}') == -1):
+        return -1
+    return buf
 
 def get_rate(output):
     output = output[output.find('{"time"'):output.find('}}}')+3:1]
@@ -142,5 +117,12 @@ def get_time(output):
     output = output[output.find('{"time'):output.find('}}}')+3:1]
     update_time = json.loads(output).get('time').get('updateduk')
     return update_time
-    
+
+def display(info):
+    lcd_gpio.clear_display()
+    lcd_gpio.return_home()
+    for i in info:
+        val = ord(i)
+        lcd_gpio.write_LCD(val)
+     
 main()
